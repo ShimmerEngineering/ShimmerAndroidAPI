159dbb35803c4af8971010f10676c9bccf162c92
jyong15 <jos15@yahoo.com>
2017-04-26 13:41:30 +0800

    Deleted: com.shimmerresearch.driver.Configuration.setTooLegacyObjectClusterSensorNames();

--- ShimmerAndroidInstrumentDriver/multiShimmerTemplate/src/main/java/com/shimmerresearch/multishimmertemplate/MainActivity.java
+++ ShimmerAndroidInstrumentDriver/multiShimmerTemplate/src/main/java/com/shimmerresearch/multishimmertemplate/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        com.shimmerresearch.driver.Configuration.setTooLegacyObjectClusterSensorNames();
+
         Intent intent=new Intent(this, MultiShimmerTemplateService.class);
        	startService(intent);
         
