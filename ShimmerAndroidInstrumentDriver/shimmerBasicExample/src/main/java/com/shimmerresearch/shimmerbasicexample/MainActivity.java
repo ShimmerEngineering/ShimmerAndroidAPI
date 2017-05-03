2ad4b2449823492a381f9d654e41cbfeb305afd8
jyong15 <jos15@yahoo.com>
2017-05-03 14:27:27 +0800

    AA-6 Shimmer Basic Example

--- ShimmerAndroidInstrumentDriver/shimmerBasicExample/src/main/java/com/shimmerresearch/shimmerbasicexample/MainActivity.java
+++ ShimmerAndroidInstrumentDriver/shimmerBasicExample/src/main/java/com/shimmerresearch/shimmerbasicexample/MainActivity.java
@@ -1,13 +1,47 @@
 package com.shimmerresearch.shimmerbasicexample;
 
+import android.bluetooth.BluetoothDevice;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.shimmerresearch.android.manager.ShimmerBluetoothManagerAndroid;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    ShimmerBluetoothManagerAndroid btManager;
+    Handler mHandler;
+    BluetoothDevice shimmerBtDevice;
+    final static String shimmerBtAdd = "00:06:66:66:96:86";  //Put the address of the Shimmer device you want to connect here
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btManager = new ShimmerBluetoothManagerAndroid(this, mHandler);
+
     }
+
+    @Override
+    protected void onStart() {
+
+        //Connect the Shimmer through Bluetooth Address
+        btManager.connectShimmerTroughBTAddress(shimmerBtAdd);
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+
+        //Disconnect Shimmer device when app is stopped
+        btManager.disconnectAllDevices();
+
+        super.onStop();
+    }
+
+
 }
