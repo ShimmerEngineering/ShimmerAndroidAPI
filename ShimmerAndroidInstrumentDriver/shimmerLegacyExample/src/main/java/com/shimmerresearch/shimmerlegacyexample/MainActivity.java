c27c1f1a4c00ae467cf53dd45a78b6aede074e83
jyong15 <jos15@yahoo.com>
2017-10-12 17:53:43 +0800

    AA-171
    Added Shimmer Legacy Example for Shimmer2r's. Connection to Shimmer2r now working.

---
+++ ShimmerAndroidInstrumentDriver/shimmerLegacyExample/src/main/java/com/shimmerresearch/shimmerlegacyexample/MainActivity.java
@@ -0,0 +1,89 @@
+package com.shimmerresearch.shimmerlegacyexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.shimmerresearch.android.guiUtilities.ShimmerBluetoothDialog;
+import com.shimmerresearch.android.manager.ShimmerBluetoothManagerAndroid;
+import com.shimmerresearch.bluetooth.ShimmerBluetooth;
+import com.shimmerresearch.driver.CallbackObject;
+import com.shimmerresearch.driver.ObjectCluster;
+
+import static com.shimmerresearch.android.guiUtilities.ShimmerBluetoothDialog.EXTRA_DEVICE_ADDRESS;
+
+public class MainActivity extends AppCompatActivity {
+
+    ShimmerBluetoothManagerAndroid btManager;
+    String shimmerBtAdd = "";
+    final static String LOG_TAG = "ShimmerLegacyExample";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            btManager = new ShimmerBluetoothManagerAndroid(this, mHandler);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch(msg.what) {
+
+                case ShimmerBluetooth.MSG_IDENTIFIER_STATE_CHANGE:
+
+                    ShimmerBluetooth.BT_STATE state = null;
+                    String macAddress = "";
+
+                    if (msg.obj instanceof ObjectCluster) {
+                        state = ((ObjectCluster) msg.obj).mState;
+                        macAddress = ((ObjectCluster) msg.obj).getMacAddress();
+                    } else if (msg.obj instanceof CallbackObject) {
+                        state = ((CallbackObject) msg.obj).mState;
+                        macAddress = ((CallbackObject) msg.obj).mBluetoothAddress;
+                    }
+
+                    switch(state) {
+                        case CONNECTED:
+                            Log.i(LOG_TAG, "Device Connected");
+                            break;
+                    }
+
+                    break;
+            }
+
+            super.handleMessage(msg);
+        }
+    };
+
+    public void selectDevice(View v) {
+        Intent intent = new Intent(getApplicationContext(), ShimmerBluetoothDialog.class);
+        startActivityForResult(intent, ShimmerBluetoothDialog.REQUEST_CONNECT_SHIMMER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+                btManager.disconnectAllDevices();   //Disconnect all devices first
+                //Get the Bluetooth mac address of the selected device:
+                String macAdd = data.getStringExtra(EXTRA_DEVICE_ADDRESS);
+                btManager.connectShimmerThroughBTAddress(macAdd);   //Connect to the selected device
+                shimmerBtAdd = macAdd;
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
