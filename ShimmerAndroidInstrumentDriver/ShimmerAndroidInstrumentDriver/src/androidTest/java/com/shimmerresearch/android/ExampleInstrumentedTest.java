3a0572a8cb189a17cbccaffd71a0b6c6564890c5
Jong Chern <jongchern@gmail.com>
2018-10-10 10:38:34 +0800

    AA-230 setting up the android unit test

--- ShimmerAndroidInstrumentDriver/ShimmerAndroidInstrumentDriver/src/androidTest/java/com/shimmerresearch/android/ExampleInstrumentedTest.java
+++ ShimmerAndroidInstrumentDriver/ShimmerAndroidInstrumentDriver/src/androidTest/java/com/shimmerresearch/android/ExampleInstrumentedTest.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.*;
 
@@ -15,18 +17,26 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void testA() throws Exception {
         // Context of the app under test.
-        assertTrue(false);
+        assertTrue(true);
         System.out.println("unit test a");
     }
 
     @Test
-    public void temp() throws Exception {
+    public void testB() throws Exception {
         // Context of the app under test.
-        assertTrue(false);
+        assertTrue(true);
         System.out.println("unit test b");
+    }
+
+    @Test
+    public void testC() throws Exception {
+        // Context of the app under test.
+        assertTrue(true);
+        System.out.println("unit test c");
     }
 }
