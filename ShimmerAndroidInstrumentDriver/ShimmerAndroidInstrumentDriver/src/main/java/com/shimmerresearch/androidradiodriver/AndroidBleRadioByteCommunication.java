14a220364c88ce2be270bea5d0234dfe06ab2163
Jong Chern <jongchern@gmail.com>
2021-09-02 23:12:02 +0800

    first steps towards getting BLE working for Verisense

---
+++ ShimmerAndroidInstrumentDriver/ShimmerAndroidInstrumentDriver/src/main/java/com/shimmerresearch/androidradiodriver/AndroidBleRadioByteCommunication.java
@@ -0,0 +1,25 @@
+package com.shimmerresearch.androidradiodriver;
+
+import com.shimmerresearch.verisense.communication.AbstractByteCommunication;
+
+public class AndroidBleRadioByteCommunication extends AbstractByteCommunication {
+    @Override
+    public void connect() {
+
+    }
+
+    @Override
+    public void disconnect() {
+
+    }
+
+    @Override
+    public void writeBytes(byte[] bytes) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
