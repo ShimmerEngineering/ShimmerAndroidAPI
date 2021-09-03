fd229b6cca6c3b17f6cb8e7c819b0d0bb06cedab
Jong Chern <jongchern@gmail.com>
2021-09-03 18:08:34 +0800

    ok some basics working now

--- ShimmerAndroidInstrumentDriver/ShimmerAndroidInstrumentDriver/src/main/java/com/shimmerresearch/androidradiodriver/AndroidBleRadioByteCommunication.java
+++ ShimmerAndroidInstrumentDriver/ShimmerAndroidInstrumentDriver/src/main/java/com/shimmerresearch/androidradiodriver/AndroidBleRadioByteCommunication.java
@@ -1,11 +1,128 @@
 package com.shimmerresearch.androidradiodriver;
 
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.content.Context;
+import android.os.Build;
+import android.widget.Toast;
+
 import com.shimmerresearch.verisense.communication.AbstractByteCommunication;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import com.clj.fastble.BleManager;
+import com.clj.fastble.callback.BleGattCallback;
+import com.clj.fastble.callback.BleNotifyCallback;
+import com.clj.fastble.callback.BleWriteCallback;
+import com.clj.fastble.data.BleDevice;
+import com.clj.fastble.exception.BleException;
+
 public class AndroidBleRadioByteCommunication extends AbstractByteCommunication {
+    BleDevice mBleDevice;
+    String TxID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+    String RxID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
+    String ServiceID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+    UUID sid = UUID.fromString(ServiceID);
+    UUID txid = UUID.fromString(TxID);
+    UUID rxid = UUID.fromString(RxID);
+    String mMac;
+
+    //"DA:A6:19:F0:4A:D7"
+    //"E7:45:2C:6D:6F:14"
+    public AndroidBleRadioByteCommunication(String mac) {
+        mMac = mac;
+    }
+
     @Override
     public void connect() {
+        BleManager.getInstance().connect(mMac, new BleGattCallback() {
+            @Override
+            public void onStartConnect() {
 
+            }
+
+            @Override
+            public void onConnectFail(BleDevice bleDevice, BleException exception) {
+                if (mByteCommunicationListener != null) {
+                    mByteCommunicationListener.eventDisconnected();
+                }
+            }
+
+            @Override
+            public void onConnectSuccess(BleDevice bleDevice, BluetoothGatt gatt, int status) {
+                if (mByteCommunicationListener != null) {
+                    mByteCommunicationListener.eventConnected();
+                }
+                mBleDevice = bleDevice;
+                startServiceS(bleDevice);
+                System.out.println(bleDevice.getMac() + " Connected");
+            }
+
+            @Override
+            public void onDisConnected(boolean isActiveDisConnected, BleDevice bleDevice, BluetoothGatt gatt, int status) {
+                System.out.println();
+            }
+        });
+    }
+
+    public void startServiceS(BleDevice bleDevice){
+        List<BluetoothGattService> services = BleManager.getInstance().getBluetoothGattServices(bleDevice);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+
+
+            for (BluetoothGattService service:services) {
+                System.out.println(service.getUuid());
+                if (service.getUuid().compareTo(sid)==0){
+                    for (BluetoothGattCharacteristic characteristic : service.getCharacteristics()) {
+                        if (characteristic.getUuid().compareTo(txid)==0){
+                            //newConnectedBLEDevice(bleDevice,characteristic);
+                            CharSequence text = "TXID!";
+                        }
+                        else if (characteristic.getUuid().compareTo(rxid)==0){
+                            newConnectedBLEDevice(bleDevice,characteristic);
+                            CharSequence text = "RxID!";
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    public void newConnectedBLEDevice(final BleDevice bleDevice, final BluetoothGattCharacteristic characteristic) {
+
+
+        int count = 1;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            BleManager.getInstance().notify(
+                    bleDevice,
+                    characteristic.getService().getUuid().toString(),
+                    characteristic.getUuid().toString(),
+                    new BleNotifyCallback() {
+
+
+                        @Override
+                        public void onNotifySuccess() {
+
+                        }
+
+                        @Override
+                        public void onNotifyFailure(BleException exception) {
+
+                        }
+
+                        @Override
+                        public void onCharacteristicChanged(byte[] data) {
+                            if (mByteCommunicationListener != null) {
+                                mByteCommunicationListener.eventNewBytesReceived(data);
+                            }
+                        }
+                    });
+        }
     }
 
     @Override
@@ -15,7 +132,17 @@ public class AndroidBleRadioByteCommunication extends AbstractByteCommunication 
 
     @Override
     public void writeBytes(byte[] bytes) {
+        BleManager.getInstance().write(mBleDevice, sid.toString(), txid.toString(), bytes, new BleWriteCallback() {
+            @Override
+            public void onWriteSuccess(int current, int total, byte[] justWrite) {
+                System.out.println("Write Success");
+            }
 
+            @Override
+            public void onWriteFailure(BleException exception) {
+                System.out.println("Write Fail");
+            }
+        });
     }
 
     @Override
---
+++ ShimmerAndroidInstrumentDriver/shimmerBLEBasicExample/src/main/java/shimmerresearch/com/shimmerbasicexample/MainActivity.java
@@ -0,0 +1,162 @@
+package shimmerresearch.com.shimmerbasicexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.shimmerresearch.android.Shimmer;
+import com.shimmerresearch.android.guiUtilities.ShimmerBluetoothDialog;
+import com.shimmerresearch.androidradiodriver.AndroidBleRadioByteCommunication;
+import com.shimmerresearch.bluetooth.ShimmerBluetooth;
+import com.shimmerresearch.driver.CallbackObject;
+import com.shimmerresearch.driver.Configuration;
+import com.shimmerresearch.driver.FormatCluster;
+import com.shimmerresearch.driver.ObjectCluster;
+import com.shimmerresearch.exceptions.ShimmerException;
+import com.shimmerresearch.verisense.VerisenseDevice;
+import com.shimmerresearch.verisense.communication.VerisenseProtocolByteCommunication;
+import com.clj.fastble.BleManager;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.shimmerresearch.android.guiUtilities.ShimmerBluetoothDialog.EXTRA_DEVICE_ADDRESS;
+
+public class MainActivity extends Activity {
+
+    private final static String LOG_TAG = "ShimmerBasicExample";
+    Shimmer shimmer;
+
+    AndroidBleRadioByteCommunication radio1 = new AndroidBleRadioByteCommunication("E7:45:2C:6D:6F:14");
+    VerisenseProtocolByteCommunication protocol1 = new VerisenseProtocolByteCommunication(radio1);
+    VerisenseDevice device1 = new VerisenseDevice();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BleManager.getInstance().init(getApplication());
+        shimmer = new Shimmer(mHandler);
+    }
+
+    public void connectDevice(View v) {
+        device1.setProtocol(Configuration.COMMUNICATION_TYPE.BLUETOOTH, protocol1);
+        try {
+            device1.connect();
+        } catch (ShimmerException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }
+
+    public void disconnectDevice(View v) {
+        if (shimmer!=null){
+            shimmer.disconnect();
+        }
+    }
+
+    public void startStreaming(View v) throws InterruptedException, IOException{
+        protocol1.readStatus();
+    }
+
+    public void stopStreaming(View v) throws IOException{
+        shimmer.stopStreaming();
+    }
+
+
+    /**
+     * Messages from the Shimmer device including sensor data are received here
+     */
+    Handler mHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch (msg.what) {
+                case ShimmerBluetooth.MSG_IDENTIFIER_DATA_PACKET:
+                    if ((msg.obj instanceof ObjectCluster)) {
+
+                        //Print data to Logcat
+                        ObjectCluster objectCluster = (ObjectCluster) msg.obj;
+
+                        //Retrieve all possible formats for the current sensor device:
+                        Collection<FormatCluster> allFormats = objectCluster.getCollectionOfFormatClusters(Configuration.Shimmer3.ObjectClusterSensorName.TIMESTAMP);
+                        FormatCluster timeStampCluster = ((FormatCluster)ObjectCluster.returnFormatCluster(allFormats,"CAL"));
+                        double timeStampData = timeStampCluster.mData;
+                        Log.i(LOG_TAG, "Time Stamp: " + timeStampData);
+                        allFormats = objectCluster.getCollectionOfFormatClusters(Configuration.Shimmer3.ObjectClusterSensorName.ACCEL_LN_X);
+                        FormatCluster accelXCluster = ((FormatCluster)ObjectCluster.returnFormatCluster(allFormats,"CAL"));
+                        if (accelXCluster!=null) {
+                            double accelXData = accelXCluster.mData;
+                            Log.i(LOG_TAG, "Accel LN X: " + accelXData);
+                        }
+                    }
+                    break;
+                case Shimmer.MESSAGE_TOAST:
+                    /** Toast messages sent from {@link Shimmer} are received here. E.g. device xxxx now streaming.
+                     *  Note that display of these Toast messages is done automatically in the Handler in {@link com.shimmerresearch.android.shimmerService.ShimmerService} */
+                    Toast.makeText(getApplicationContext(), msg.getData().getString(Shimmer.TOAST), Toast.LENGTH_SHORT).show();
+                    break;
+                case ShimmerBluetooth.MSG_IDENTIFIER_STATE_CHANGE:
+                    ShimmerBluetooth.BT_STATE state = null;
+                    String macAddress = "";
+
+                    if (msg.obj instanceof ObjectCluster) {
+                        state = ((ObjectCluster) msg.obj).mState;
+                        macAddress = ((ObjectCluster) msg.obj).getMacAddress();
+                    } else if (msg.obj instanceof CallbackObject) {
+                        state = ((CallbackObject) msg.obj).mState;
+                        macAddress = ((CallbackObject) msg.obj).mBluetoothAddress;
+                    }
+
+                    switch (state) {
+                        case CONNECTED:
+                            break;
+                        case CONNECTING:
+                            break;
+                        case STREAMING:
+                            break;
+                        case STREAMING_AND_SDLOGGING:
+                            break;
+                        case SDLOGGING:
+                            break;
+                        case DISCONNECTED:
+                            break;
+                    }
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    /**
+     * Get the result from the paired devices dialog
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+                //Get the Bluetooth mac address of the selected device:
+                String macAdd = data.getStringExtra(EXTRA_DEVICE_ADDRESS);
+                shimmer = new Shimmer(mHandler);
+                shimmer.connect(macAdd, "default");                  //Connect to the selected device
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+}
